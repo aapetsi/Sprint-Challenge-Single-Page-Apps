3fd2e3d5239886bce84f14ea995d7e95382f0355
aapetsi <apetsiampiah@gmail.com>
2019-09-20 10:53:48 +0000

    Wired up Search form to query character by name

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import CharacterCard from "./CharacterCard";
+import Axios from "axios";
 
 const SearchForm = () => {
+  const [character, setCharacter] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  const [errorState, setErrorState] = useState("");
+
+  const handleInputChange = e => {
+    let text = e.target.value;
+    setSearchText(text);
+  };
+
   const handleFormSubmit = e => {
     e.preventDefault();
+    setErrorState("");
+    Axios.get(`https://rickandmortyapi.com/api/character/?name=${searchText}`)
+      .then(res => {
+        setCharacter([...res.data.results]);
+      })
+      .catch(err => {
+        let errorMessage = err.message;
+        setErrorState("Character not found");
+        console.log(errorMessage);
+      });
   };
+
+  const userToReturn =
+    character.length === 0 ? (
+      <p>Enter a user to begin search</p>
+    ) : (
+      character.map(character => (
+        <CharacterCard
+          key={character.id}
+          image={character.image}
+          name={character.name}
+          status={character.status}
+          species={character.species}
+          location={character.location}
+          origin={character.location}
+          episode={character.episode}
+        />
+      ))
+    );
+
   return (
     <div>
       <form onSubmit={handleFormSubmit}>
-        <input type="search" />
+        <input
+          autoFocus={true}
+          name="searchField"
+          type="text"
+          onChange={handleInputChange}
+        />
         <button type="submit">Search</button>
       </form>
+      {errorState ? errorState : userToReturn}
     </div>
   );
 };
